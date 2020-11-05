139fa46bcc4736cf40a02d418dd67f2e096c462e
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2020-11-05 10:55:03 +0700

    needs fixing

--- index.js
+++ index.js
@@ -4,23 +4,31 @@ const tz = require('moment-timezone');
 const chalk = require('chalk');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-const { timezone, format, clockchannel, updateinterval, token} = require('./config.js');
+process.on("unhandledRejection",(error,promise) => {
+  console.error(error)
+})
+const { timezone, format, clockchannel, updateinterval, token,bitrate,userLimit,guild_id,position } = require('./config.js');
 
 //'ready' event
-client.once('ready', () => {
+client.once('ready',async () => {
   //init time
   const timeNow = moment().tz(timezone).format(format);
-  //define clockChannel
-  const clockChannel = client.channels.cache.get(clockchannel);
-  //initial update
-  clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
-    .catch(console.error);
+  //No position, just in case the channel moved
+  let clockChannel = client.channels.cache.find(channel => channel.bitrate === bitrate && channel.userLimit === channel.userLimit) 
+  clockChannel.clone({ bitrate,userLimit,position,type:"voice",name:`🕒 ${timeNow}` })
+  .then(async channel => {
+    if (clockChannel) await clockChannel.delete()
+    clockChannel =  channel
+  })
+  
   //set the interval
-  setInterval(() => {
+  setInterval(async () => {
     const timeNowUpdate = moment().tz(timezone).format(format);
-    clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update')
-      .catch(console.error);
+    clockChannel.clone({ bitrate,userLimit,position,type:"voice",name:`🕒 ${timeNowUpdate}` })
+    .then(async channel => {
+      await clockChannel.delete()
+      clockChannel =  channel
+    })
   }, updateinterval);
   //tells if it is ready
 	console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
