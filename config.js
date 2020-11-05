8dbad20bc93fb1555499cf29fd3b5a94939ca584
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2020-11-05 10:50:00 +0700

    Rollback

--- config.js
+++ config.js
@@ -1,17 +1,9 @@
 module.exports = {
   //all fields are **required**
   token: 'NTY3MTIzzfutyWBshRSNPdyA.sggwR0.MiuR2zs5BR_tFXIwFkqu3Ci2qKr', // Your bot's token
-  position:0, //channel postition
-  guild_id:"123456789012345678", //server id
+  clockchannel: '666962545675862036', // ID of a voice channel that used to display the time
   timezone: 'Asia/Jakarta', // Timezone (take a look at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List)
   format: 'HH:mm (z)', // Clock format, leave this default seting for 24h format, read more at https://momentjs.com/docs/#/displaying/format/
-<<<<<<< HEAD
-  updateinterval: 60000,
-=======
-  updateinterval: 60000, // Update interval (in miliseconds), 60000 (1 minute) is recommended. So the clock will update every 1 minute
->>>>>>> parent of fdbb54a... Welp, Discord is ratelimiting us!
+  updateinterval: 600000, // Discord is ratelimiting us for 10 minutes!
   //[ON WORK, IGNORE THIS FIELD] dev: '400581909912223744', // Developer's ID for sending the errors
-  // Used to find the channel
-  bitrate:73643,
-  userLimit:83
 }
--- index.js
+++ index.js
@@ -4,31 +4,23 @@ const tz = require('moment-timezone');
 const chalk = require('chalk');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-process.on("unhandledRejection",(error,promise) => {
-  console.error(error)
-})
-const { timezone, format, clockchannel, updateinterval, token,bitrate,userLimit,guild_id,position } = require('./config.js');
+
+const { timezone, format, clockchannel, updateinterval, token} = require('./config.js');
 
 //'ready' event
-client.once('ready',async () => {
+client.once('ready', () => {
   //init time
   const timeNow = moment().tz(timezone).format(format);
-  //No position, just in case the channel moved
-  let clockChannel = client.channels.cache.find(channel => channel.bitrate === bitrate && channel.userLimit === channel.userLimit) 
-  clockChannel.clone({ bitrate,userLimit,position,type:"voice",name:`🕒 ${timeNow}` })
-  .then(async channel => {
-    if (clockChannel) await clockChannel.delete()
-    clockChannel =  channel
-  })
-  
+  //define clockChannel
+  const clockChannel = client.channels.cache.get(clockchannel);
+  //initial update
+  clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
+    .catch(console.error);
   //set the interval
-  setInterval(async () => {
+  setInterval(() => {
     const timeNowUpdate = moment().tz(timezone).format(format);
-    clockChannel.clone({ bitrate,userLimit,position,type:"voice",name:`🕒 ${timeNowUpdate}` })
-    .then(async channel => {
-      await clockChannel.delete()
-      clockChannel =  channel
-    })
+    clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update')
+      .catch(console.error);
   }, updateinterval);
   //tells if it is ready
 	console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
